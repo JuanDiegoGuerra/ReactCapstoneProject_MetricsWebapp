78e42e9de6570b702f534ff36ffd604c832e4281
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-07 02:33:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import CoinsList from './components/CoinsList';
+
 function App() {
   return (
     <div className="App text-zinc-600">
       Hola REACT!
+      <CoinsList />
     </div>
   );
 }
