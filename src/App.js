7207ee2c8af69ff76d584939482daecc27328b0c
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-07 02:46:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import CoinsList from './components/CoinsList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Coins from './routes/Coins';
+import SpecificCoin from './routes/SpecificCoin';
 
 function App() {
   return (
-    <div className="App text-zinc-600">
-      Hola REACT!
-      <CoinsList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Coins />} />
+          <Route path="coin" element={<SpecificCoin />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
