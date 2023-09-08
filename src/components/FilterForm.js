48fd5cd07316f73d2feef9cbb38981c968ff1ee3
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-08 03:27:24 -0500

    Update FilterForm.js

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -24,7 +24,7 @@ function FilterForm() {
     <>
       <form className="w-full flex items-center justify-center bg-blue-1 p-5">
         <label htmlFor="name" className="w-2/3">
-          <input onChange={(e) => { handleChange(e); }} type="text" id="name" name="name" placeholder="Filter by name..." className="w-full flex text-text-color justify-center border-2 outline-0 border-text-color rounded-lg px-4 py-2 bg-blue-1 placeholder:text-text-color" />
+          <input onChange={(e) => { handleChange(e); }} type="text" id="name" name="name" placeholder="Filter by crypto name..." className="w-full flex text-text-color justify-center border-2 outline-0 border-text-color rounded-lg px-4 py-2 bg-blue-1 placeholder:text-text-color" />
         </label>
       </form>
       <p className="bg-blue-5 px-6 py-1 text-text-color lato font-bold">{filtering ? 'Cryptocurrency found' : 'Cryptos'}</p>
