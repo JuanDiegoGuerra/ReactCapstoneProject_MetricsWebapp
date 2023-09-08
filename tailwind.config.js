044b403843882f5b0f9434b78322fbb16f53dd4a
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-08 02:22:11 -0500

    Update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-color': '#F5F7FB',
+        'blue-1': '#2D4573',
+      },
+      backgroundColor: {
+        'blue-1': '#2D4573',
+        'blue-2': '#35548B',
+        'blue-3': '#4064A6',
+        'blue-4': '#4369B2',
+        'blue-5': '#5688E4',
+      },
+    },
   },
   plugins: [],
 };
